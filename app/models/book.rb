c8652cc5fc12c2f90444b1701a6d9ddf50ad5060
alperenbozkurt <bozkurttalperen@gmail.com>
2016-08-19 17:38:06 +0300

    edit writers for admin

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :comments, dependent: :destroy
 	belongs_to :category
-	belongs_to :writer
+	belongs_to :writer, dependent: :destroy
 
 end
--- app/models/writer.rb
+++ app/models/writer.rb
@@ -1,5 +1,5 @@
 class Writer < ApplicationRecord
-has_many :Books
+has_many :Books, dependent: :destroy
 validates :name, presence: true
 
 end
