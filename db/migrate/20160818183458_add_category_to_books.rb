9f29c3673f2c9347c1daef58b318601ae830c1c2
alperenbozkurt <bozkurttalperen@gmail.com>
2016-08-19 11:54:28 +0300

    schema çözüldü

--- db/migrate/20160818183458_add_category_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToBooks < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :books, :category, foreign_key: true
-  end
-end
--- db/migrate/20160819065056_add_comments_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentsToBooks < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :books, :comments, foreign_key: true
-  end
-end
